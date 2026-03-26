e704e16f30d1518f350347bfd99d0c1f436eb71b
Pedro Adam Python <canalzueiragameso@gmail.com>
2026-03-26 20:23:19 -0300

    O que é Váriavel? E pra que serve?
    Added variable examples and explanations in Python.

--- variaveis.py
+++ variaveis.py
@@ -1,1 +1,16 @@
+# Como funciona as Váriaveis!
 
+nome = "Python"  # nome é a váriavel, e Python é a Str.(String) - String seria um texto.
+idade = 22       # idade seria a váriavel, e o número int(Inteiro), seria o valor que esta variavel esta recebendo.
+altura = 1.65    # altura é uma variavel, e o número float(fluatuante), é o valor desta variavel.
+peso = 63.6      # mas um número float. Que é o valor da variavel "peso". - Float números que possuem casas decimais.
+
+#Exemplos:
+
+print(f"Meu nome é {nome}, tenho {idade} anos, tenho {altura} de altura. E peso {peso} Kg. ") # as variaveis recebem o valor das strings.
+
+# nome,idade,altura e peso são as váriaveis!
+
+print("Meu nome é", nome, "tenho", idade, "anos, tenho", altura, "de altura. E peso", peso, "Kg.")
+# # De forma mais simplificada, e que funciona mas é melhor usar a f-string.
+# nome, idade, altura, peso ainda são as variaveis!
