19c95c9c75fab9cf8c82deaab0bd7702a82ba503
Pedro Adam Python <canalzueiragameso@gmail.com>
2026-03-27 13:58:52 -0300

    O Input
    Explicando sobre o Input, pra iniciantes em Python.

--- input.py
+++ input.py
@@ -1,1 +1,10 @@
+#O Input
+Nome = input("Qual seu nome? ") # O input aqui neste caso serve pra perguntar pra pessoa: Qual seu nome?
 
+Idade = input("Qual a sua idade? ") # Aqui pra perguntar a idade da pessoa.
+
+#Basicamente ele serve pra perguntar pra pessoa, e pra perguntar é só abrir parenteses e colchetes e escrever ali dentro a pergunta.
+
+print(f"Seu nome é {Nome} e sua idade é de {Idade} anos.") 
+# E pra mandar uma resposta completa, é só abrir uma print e colocar as variaveis 
+# dentro do {} usando F-strings, ou se não usando outras formas pra colocar variaveis.
