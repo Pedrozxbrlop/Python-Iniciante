7a2912a76081a3776d4915b5c2a7537fb80a0f7b
Pedro Adam Python <canalzueiragameso@gmail.com>
2026-03-27 22:23:43 -0300

    O Input
    Como usar e quando usar o Input.

--- input.py
+++ input.py
@@ -1,10 +1,9 @@
 #O Input
-Nome = input("Qual seu nome? ") #O input aqui neste caso serve pra perguntar pra pessoa: Qual seu nome?
+Nome = input("Qual seu nome? ") #O input aqui neste caso serve pra perguntar pra a pessoa: Qual seu nome?
 
 Idade = input("Qual a sua idade? ") # Aqui pra perguntar a idade da pessoa.
 
-#Basicamente ele serve pra perguntar pra pessoa, e pra perguntar é só abrir parenteses e colchetes e escrever ali dentro a pergunta.
+#Basicamente ele serve pra perguntar pra pessoa, e pra perguntar é só abrir parenteses e aspas e escrever ali dentro a pergunta.
 
-print(f"Seu nome é {Nome} e sua idade é de {Idade} anos.") 
-# E pra mandar uma resposta completa, é só abrir uma print e colocar as variaveis 
-# dentro do {} usando F-strings, ou se não usando outras formas pra colocar variaveis.
+print(f"Seu nome é {Nome} e sua idade é de {Idade} anos.") # E pra mandar uma resposta completa, é só abrir uma print e colocar as variaveis
+                                                           # dentro do {} usando F-strings, ou se não de outras formas de print.
